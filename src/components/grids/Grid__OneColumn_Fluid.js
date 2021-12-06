d19d0caf3e1a17a9c33c3ed76cb69e500bd06308
markgombar <git@markg.design>
2021-12-06 16:42:10 -0500

    Initial website commit

---
+++ src/components/grids/Grid__OneColumn_Fluid.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+import "../../sass/components/grids/Grid__OneColumn_Fluid.sass"
+
+export default function Grid__OneColumn_Fluid ({ children }) {
+    return (
+        <div className="grid__oneColumn-fluid">
+            {children}
+        </div>
+    )
+}
+
+Grid__OneColumn_Fluid.propTypes = {
+    children: PropTypes.node,
+}
